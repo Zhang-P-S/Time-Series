900296cbb8176fbbcee4060b1ab83079d7c18ad7
Pusheng Zhang <Zhangpusheng2021@outlook.com>
2025-08-14 09:59:11 +0800

    修改了test文件作为beartype学习测试

--- test.py
+++ test.py
@@ -1,26 +1,9 @@
-import csv
-import pandas as pd
-import matplotlib.pyplot as plt
-import os
+from beartype import beartype
 
-df = pd.read_csv('data\ETTh1.csv')
-
-df['label'] = 'normal'
-df.loc[2,'label'] = 'anormaly'
-print(df.head())
-df= df.set_index('date')
-print(df.iloc[:,0].head())
-df.reset_index(inplace=True)
-timestamp = pd.to_datetime(df['date'])
-data = df.loc[:, ['HUFL']]
-plt.figure(figsize=(10, 4))
-plt.plot(timestamp, data,linewidth=0.5, color='black', label='HUFL')
-plt.xlabel('Time')
-plt.ylabel('HUFL')
-plt.grid(True)
-
-
-if not os.path.exists('fig'):
-    os.makedirs('fig')
-plt.savefig('fig/ETTh1.pdf', dpi=300, bbox_inches='tight')
-plt.show()
+@beartype
+def add(a:int,b:int) -> int:
+    return a+b
+print("qws")
+a = 1
+b = 2
+print("Result:", add(a, b))  # This will call the beartyped function
